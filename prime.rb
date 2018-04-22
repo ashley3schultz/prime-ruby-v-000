a190b7c9efb3f4c6d11f7830f535a768d73a9f6d
Ashley Schultz <ashleyeschultz@gmail.com>
2018-04-22 20:36:50 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -7,6 +7,9 @@ def prime?(number)
     (2..t).any? {|ea| (number % ea == 0) || (number % ea > 0)}
     FALSE
   else 
+    t = number - 1
+    range = (2..t).to_a
+    (2..t).any? {|ea| (number % ea == 0)
     TRUE
   end
 end
