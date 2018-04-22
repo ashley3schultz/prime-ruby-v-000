3bba6466339748002859da7b10ab836e441b3c3d
Ashley Schultz <ashleyeschultz@gmail.com>
2018-04-22 20:47:18 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,9 @@
 def prime?(number)
   t = number - 1
   range = (2..t).to_a
-  (2..t).none? {|ea| (number % ea >= 0)}
+  if (2..t).none? {|ea| (number % ea >= 0)}
+    TRUE
+  else
+    FALSE
+  end
 end
