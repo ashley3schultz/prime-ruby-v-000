ac305c1df9a3dac8fe6716141ed9c62913bdbfee
Ashley Schultz <ashleyeschultz@gmail.com>
2018-04-22 20:02:30 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,8 +4,6 @@ def prime?(number)
   (start..number).each do |no|
     (start..no).each do |num|
       if ( no % num  == 0) && num != no
-        primes.delete(no)
-        break
       end
     end
   end
