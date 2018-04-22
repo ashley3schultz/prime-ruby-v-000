26315a0cc5381cba3d44db1d62e2fee9ee05642e
Ashley Schultz <ashleyeschultz@gmail.com>
2018-04-22 20:39:31 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,13 @@
 def prime?(number)
   if number < 2 
-    FALSE
+    prime = FALSE
   elsif
     t = number - 1
     range = (2..t).to_a
     (2..t).any? {|ea| (number % ea) == 0 || (number % ea) > 0}
-    FALSE
+    prime = FALSE
   else 
-    t = number - 1
-    range = (2..t).to_a
-    (2..t).none? {|ea| (number % ea) == 0}
-    TRUE
+    prime = TRUE
   end
+  prime
 end
